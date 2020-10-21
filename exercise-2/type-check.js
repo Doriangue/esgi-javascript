a9375030fcce909186107ccb8f22e0677076e098
Dorian Gueguen <doriangueguen@orange.fr>
2020-10-21 18:14:49 +0200

    type-check1

---
+++ exercise-2/type-check.js
@@ -0,0 +1,11 @@
+function type_check_v1(val, type) {
+    if (typeof type !== 'string') return false;
+    if (typeof type === 'array') {
+        return Array.isArray(val);
+    } else if (val === null && type === 'object') {
+        return false;
+    } else if (type === 'null') {
+        return val === null;
+    }
+    return typeof val === type;
+}
